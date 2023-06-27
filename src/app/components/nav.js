eb015bc8ba519b6ab8bab4551d5562967c3f3d23
thxrhmn <rahmanmuhaemin@gmail.com>
2023-06-27 13:54:23 +0700

    adding nav

---
+++ src/app/components/nav.js
@@ -0,0 +1,26 @@
+
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <div className="my-8 mx-5">
+      <ul className="flex justify-evenly gap-x-2">
+        <li>
+          <Link href="/">About</Link>
+        </li>
+        <span>/</span>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <span>/</span>
+        <li>
+          <Link href="/project">Project</Link>
+        </li>
+        <span>/</span>
+        <li>
+          <Link href="/skill">Skill</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
