db2a9b9b40158535c7c9a66d0e809cc8306b6380
thxrhmn <rahmanmuhaemin@gmail.com>
2023-06-27 19:58:28 +0700

    adding timeline projects

--- pages/project.js
+++ pages/project.js
@@ -1,11 +1,60 @@
 import Nav from "@/app/components/nav";
 import { data } from "@/app/data";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Project() {
   return (
     <main className="md:w-[500px] mx-auto font-mono">
       <Nav />
       <div className="my-8 mx-5">
+        <h2 className="font-bold text-center mb-4">My Projects</h2>
+        <div className="">
+          <ol className="relative border-l border-gray-200 dark:border-gray-700">
+            {data.projects.map((item, idx) => (
+              <li key={idx} className="mb-10 ml-4">
+                <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-black dark:bg-black"></div>
+                <time className="mb-1 text-sm font-normal leading-none italic">
+                  {item.process}
+                </time>
+                <h3 className="text-lg">{item.name}</h3>
+                <p className="mb-4 text-base font-normal">{item.desc}</p>
+                <div className="flex">
+                  <h5 className="text-sm">stacks:</h5>
+                  <ul className="flex text-sm">
+                    {item.stacks.map((item, idx) => (
+                      <li className="px-2 underline" key={idx}>
+                        {item.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="flex">
+                  <Link href={item.linkGH}>
+                    <Image
+                      className="rounded-full"
+                      src="/github.svg"
+                      alt="Github Logo"
+                      width={15}
+                      height={15}
+                    />
+                  </Link>
+                  <Link href={item.linkProd}>
+                    <Image
+                      className="rounded-full"
+                      src="/network.svg"
+                      alt="Network Logo"
+                      width={15}
+                      height={15}
+                    />
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+      {/* <div className="my-8 mx-5">
         <h2 className="font-bold text-center">My Projects</h2>
         <div className="">
           {data.projects.map((item) => (
@@ -17,7 +66,7 @@ export default function Project() {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </main>
   );
 }
