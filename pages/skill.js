9e5b66cde40fa53a7676336d5ea2b0e1e28932e5
thxrhmn <rahmanmuhaemin@gmail.com>
2023-06-27 15:30:41 +0700

    fix Skills

--- pages/skill.js
+++ pages/skill.js
@@ -6,7 +6,7 @@ export default function Skill() {
     <main className="md:w-[500px] mx-auto font-mono">
       <Nav />
       <div className="my-8 mx-5">
-        <h2 className="text-center mb-4 font-bold">Programming Skill</h2>
+        <h2 className="text-center mb-4 font-bold">Programming Skills</h2>
         <ul className="text-center">
           {data.programmingStack.map((item) => (
             <li key={item.name}>{item.name}</li>
