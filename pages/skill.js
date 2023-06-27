84bd32693fff4c18d081f02cad03879369cf42ec
thxrhmn <rahmanmuhaemin@gmail.com>
2023-06-27 20:16:33 +0700

    adding new class

--- pages/skill.js
+++ pages/skill.js
@@ -7,7 +7,7 @@ export default function Skill() {
       <Nav />
       <div className="my-8 mx-5">
         <h2 className="text-center mb-4 font-bold">Programming Skills</h2>
-        <ul className="text-center">
+        <ul className="text-center programming-skills">
           {data.programmingStack.map((item) => (
             <li key={item.name}>{item.name}</li>
           ))}
