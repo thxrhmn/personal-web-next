078fe267cb36f58cb2f7f462fb64d92cd96d26c8
thxrhmn <rahmanmuhaemin@gmail.com>
2023-06-28 13:26:57 +0700

    add isLoading when swhitching pages / routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import "../src/app/globals.css"
+import Router from "next/router"
+import { useState, useEffect } from "react"
+import Loading from "@/app/loading"
 
 export default function MyApp({ Component, pageProps}){
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", (url) => {
+      setIsLoading(true)
+    })
+
+    Router.events.on("routeChangeComplete", (url) => {
+      setIsLoading(false)
+    })
+
+    Router.events.on("routeChangeError", (url) => {
+      setIsLoading(false)
+    })
+
+  }, [Router])
+
   return (
-    <Component {...pageProps} />
+    <>
+      {isLoading && <Loading/>}
+      <Component {...pageProps} />
+    </>
   )
 }
