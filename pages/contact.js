ba0e742b0b1218f5d990d0002eaaf7d9b76429b6
thxrhmn <rahmanmuhaemin@gmail.com>
2023-06-28 08:22:22 +0700

    change & add icons

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import Nav from "@/app/components/nav";
 import { data } from "@/app/data";
+import Image from "next/image";
 
 export default function Contact() {
   return (
@@ -7,11 +8,67 @@ export default function Contact() {
       <Nav />
       <div className="my-8 mx-5">
         <h2 className="text-center mb-4 font-bold">Contacts</h2>
-        <ul className="text-center contacts">
-          <li><a href={`mailto:${data.contacts.email}`}>Email</a></li>
-          <li><a href={data.contacts.linkedin}>Linkedin</a></li>
-          <li><a href={data.contacts.github}>Github</a></li>
-          <li><a href={data.contacts.website}>Website</a></li>
+        <ul className="text-center">
+          <li className="">
+            <a className="flex contacts" href={`mailto:${data.contacts.email}`}>
+              <Image
+                className="rounded-full"
+                src={`/email.svg`}
+                alt="Email Icon"
+                width={17}
+                height={17}
+              />
+              <span>{data.contacts.email}</span>
+            </a>
+          </li>
+          <li>
+            <a className="flex contacts" href={data.contacts.linkedin}>
+              <Image
+                className="rounded-full"
+                src={`/linkedin.svg`}
+                alt="Linkedin Icon"
+                width={17}
+                height={17}
+              />
+              <span>{data.contacts.linkedin}</span>
+            </a>
+          </li>
+          <li>
+            <a className="flex contacts" href={data.contacts.github}>
+              <Image
+                className="rounded-full"
+                src={`/github.svg`}
+                alt="Github Icon"
+                width={17}
+                height={17}
+              />
+              <span>{data.contacts.github}</span>
+            </a>
+          </li>
+          <li>
+            <a className="flex contacts" href={data.contacts.website}>
+              <Image
+                className="rounded-full"
+                src={`/network.svg`}
+                alt="Web Icon"
+                width={17}
+                height={17}
+              />
+              <span>{data.contacts.website}</span>
+            </a>
+          </li>
+          <li>
+            <a className="flex contacts" href={data.contacts.telegram}>
+              <Image
+                className="rounded-full"
+                src={`/telegram.svg`}
+                alt="Web Icon"
+                width={17}
+                height={17}
+              />
+              <span>{data.contacts.telegram}</span>
+            </a>
+          </li>
         </ul>
       </div>
     </main>
